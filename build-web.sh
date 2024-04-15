9223252102d6ffca3923d8bd29d7cf8426f784ff
William Roe <git@wjlr.org.uk>
2024-04-15 18:22:34 +0100

    Note about needing more memory in webasm possibly

--- build-web.sh
+++ build-web.sh
@@ -22,5 +22,6 @@ set -x
 compile_flags="-I../../vendor/SDL2_gfx $(pkg-config --cflags sdl2,SDL2_image,SDL2_ttf)"
 # emcc -c ../../main.c ../../vendor/SDL2_gfx/SDL2_gfxPrimitives.c ../../vendor/SDL2_gfx/SDL2_rotozoom.c ${compile_flags}
 tree assets
+# Do we need '-sALLOW_MEMORY_GROWTH=1 -sMAXIMUM_MEMORY=1gb' here?
 emcc --use-port=sdl2 --use-port=sdl2_image:formats=png,jpg --use-port=sdl2_ttf --preload-file ./assets/fonts/im_fell_roman.ttf --bind -o index.html ../../main.c ${compile_flags}
 set +x
