85bce7e5398a6763a890930a6900db212a8222b7
William Roe <git@wjlr.org.uk>
2024-04-17 23:22:36 +0100

    WIP: try to build resources into Unix binary

--- build.sh
+++ build.sh
@@ -4,10 +4,14 @@ set -eu
 
 host_os="$(uname -s)"
 
+xxd -i vertex_shader.glsl > assets/vertex_shader_glsl.h
+
 echo "Building natively"
 pushd build
 
-compile_flags="-I../vendor/SDL2_gfx $(pkg-config --cflags --libs sdl2,SDL2_image,SDL2_ttf) -DUNIX"
+cc -c ../assets/vertex_shader_glsl.h -o vertex_shader_glsl.o
+
+compile_flags="-I../vendor/stb -I../vendor/SDL2_gfx $(pkg-config --cflags --libs sdl2,SDL2_image,SDL2_ttf) -DUNIX"
 
 if [[ "${host_os}" == "Darwin" ]]; then
 	rosetta_pids="$(fuser /usr/libexec/rosetta/runtime 2>/dev/null | xargs -n1)"
@@ -22,8 +26,8 @@ if [[ "${host_os}" == "Darwin" ]]; then
 fi
 
 set -x
-cc -std=c11 -g -O0 -o summoning_debug ../main.c ${compile_flags} -lm -DDEBUG
-cc -std=c11 -O3 -o summoning ../main.c ${compile_flags} -lm
+cc -std=c11 -g -O0 -o summoning_debug ../main.c ${compile_flags} vertex_shader_glsl.o -lm -DDEBUG
+cc -std=c11 -O3 -o summoning ../main.c ${compile_flags} -lm vertex_shader_glsl.o
 set +x
 
 if [[ "${host_os}" == "Darwin" ]]; then
